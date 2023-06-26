f88215d70697b5d9516db43aeb985dedff3bf317
Diego Ramos <diegorbrito9@gmail.com>
2023-06-26 12:07:29 -0300

    new: criando pagina inicial
    description:
    criando estrutura html
    criando estilos da pagina
    inportando componente de input na pagina inicial

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,29 @@
+import { ChangeEvent, useState } from "react";
+import { Input } from "@/components/Input";
+import styles from "@/styles/pages/home/styles.module.scss";
+
 export default function Home() {
-  return <div>hello world!</div>;
+  const [url, setUrl] = useState<string>("");
+
+  const handleInputChange = ({ target }: ChangeEvent<HTMLInputElement>) => {
+    return setUrl(target.value);
+  };
+
+  return (
+    <main className={styles.mainContainer}>
+      <div className={styles.content}>
+        <h1>Encurtador de url</h1>
+
+        <form>
+          <Input
+            inputLabel="Digite uma url"
+            inputPlaceholder="youtube.com"
+            handleInputChange={handleInputChange}
+            urlValue={url}
+          />
+          <button className={styles.buttonSubmit}>Encurtar</button>
+        </form>
+      </div>
+    </main>
+  );
 }
