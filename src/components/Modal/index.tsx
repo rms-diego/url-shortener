936c1a66d48aff294857028f99c736f5bc47be32
Diego Ramos <diegorbrito9@gmail.com>
2023-06-28 14:08:58 -0300

    feat: criando o modal e seus estilos
    description:
    criando a estrutura html do modal
    criando os estilos do modal
    instalando a biblioteca 'react-icons'
    aplicando na pagina principal a logica de fazer o post para a criação da url

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,35 @@
+import styles from "./styles.module.scss";
+
+import { UrlCreatedType } from "@/services/apiServices/url/saveUrl";
+import Link from "next/link";
+
+import { FaWindowClose } from "react-icons/fa";
+
+interface ModalProps {
+  urlCreated: UrlCreatedType;
+  toggleOpenAndCloseModal: () => void;
+}
+
+export function Modal({ urlCreated, toggleOpenAndCloseModal }: ModalProps) {
+  const domain = window.location.href;
+
+  return (
+    <div className={styles.modalContainer}>
+      <div className={styles.modalContentContainer}>
+        <FaWindowClose size={25} onClick={() => toggleOpenAndCloseModal()} />
+
+        <div className={styles.content}>
+          <h2>Url encurtada: {`${domain}${urlCreated.hash}`}</h2>
+
+          <Link
+            href={`http://${urlCreated.baseUrl}`}
+            className={styles.redirectButtonContainer}
+            target="_blank"
+          >
+            <button>Ir para o link </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,61 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import { Input } from "@/components/Input";
+
 import styles from "@/styles/pages/home/styles.module.scss";
+import { UrlCreatedType, saveUrl } from "@/services/apiServices/url/saveUrl";
+import { Modal } from "@/components/Modal";
 
 export default function Home() {
   const [url, setUrl] = useState<string>("");
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const [urlCreated, setUrlCreated] = useState<UrlCreatedType>();
 
   const handleInputChange = ({ target }: ChangeEvent<HTMLInputElement>) => {
     return setUrl(target.value);
   };
 
-  return (
-    <main className={styles.mainContainer}>
-      <div className={styles.content}>
-        <h1>Encurtador de url</h1>
+  const handleSubmitForm = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
 
-        <form>
-          <Input
-            inputLabel="Digite uma url"
-            inputPlaceholder="youtube.com"
-            handleInputChange={handleInputChange}
-            urlValue={url}
-          />
-          <button className={styles.buttonSubmit}>Encurtar</button>
-        </form>
-      </div>
-    </main>
+    const data = await saveUrl(url);
+
+    setUrl("");
+    setUrlCreated(data);
+    return setShowModal(true);
+  };
+
+  const toggleOpenAndCloseModal = () => {
+    return setShowModal(false);
+  };
+
+  return (
+    <>
+      {showModal ? (
+        <Modal
+          urlCreated={urlCreated!}
+          toggleOpenAndCloseModal={toggleOpenAndCloseModal}
+        />
+      ) : (
+        <></>
+      )}
+
+      <main className={styles.mainContainer}>
+        <div className={styles.content}>
+          <h1>Encurtador de url</h1>
+
+          <form onSubmit={handleSubmitForm}>
+            <Input
+              inputLabel="Digite uma url"
+              inputPlaceholder="youtube.com"
+              handleInputChange={handleInputChange}
+              urlValue={url}
+            />
+            <button className={styles.buttonSubmit} type="submit">
+              Encurtar
+            </button>
+          </form>
+        </div>
+      </main>
+    </>
   );
 }
