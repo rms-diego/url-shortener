4fe3f11a8f98cbda6d2561cb2e08bd8756106556
Diego Ramos <diegorbrito9@gmail.com>
2023-10-11 14:54:14 -0300

    recomeçando o projeto

--- prisma/migrations/20230627143932_create_url_table/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE `url` (
-    `id` VARCHAR(191) NOT NULL,
-    `baseUrl` VARCHAR(191) NOT NULL,
-    `shortedUrl` VARCHAR(191) NOT NULL,
-
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- prisma/migrations/20230630163506_remove_atributte_hash_from_table_url/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `hash` on the `url` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `url` DROP COLUMN `hash`;
