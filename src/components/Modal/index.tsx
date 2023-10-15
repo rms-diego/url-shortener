93e4565ea3cc6ad5ae1e52b6e35a0a637e4e2b6f
Diego Ramos <diegorbrito9@gmail.com>
2023-10-15 13:13:42 -0300

    feat: implementando login e registro no frontend

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,17 +1,65 @@
+import { ChangeEvent, FormEvent, useEffect, useState } from 'react';
 import styles from './styles.module.scss';
+import { client } from '@/utils/client';
 
 type Props = {
   isOpen: boolean;
   toggleOpen(): void;
-  title: string;
+  title: 'Login' | 'Cadastrar';
 };
 
 export function Modal({ isOpen, toggleOpen, title }: Props) {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  function handleEmailChange({ target }: ChangeEvent<HTMLInputElement>) {
+    setEmail(target.value);
+  }
+
+  function handlePasswordChange({ target }: ChangeEvent<HTMLInputElement>) {
+    setPassword(target.value);
+  }
+
+  async function handleSubmitForm(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    if (title === 'Cadastrar') {
+      const response = await client
+        .post('/auth/register', { email, password })
+        .catch(() => alert('Usuário já existe'));
+
+      setEmail('');
+      setPassword('');
+      toggleOpen();
+      // localStorage.setItem('', r)
+      return;
+    }
+
+    const data = await client
+      .post('/auth/login', { email, password })
+      .catch((error) => {
+        const { data } = error.response;
+
+        if (data.error === 'invalid password') {
+          return alert('senha inválida');
+        }
+
+        return alert('usuário não existe');
+      });
+
+    console.log(data);
+    setEmail('');
+    setPassword('');
+    toggleOpen();
+
+    return;
+  }
+
   return (
     <>
       {isOpen && (
         <div className={styles.backgroundModal}>
-          <form className={styles.formContainer}>
+          <form className={styles.formContainer} onSubmit={handleSubmitForm}>
             <section className={styles.headerFormContainer}>
               <h2>{title}</h2>
               <button
@@ -25,15 +73,28 @@ export function Modal({ isOpen, toggleOpen, title }: Props) {
 
             <label htmlFor="">
               Email
-              <input type="email" placeholder="Adicione seu link aqui" />
+              <input
+                type="email"
+                placeholder="Adicione seu link aqui"
+                onChange={handleEmailChange}
+                value={email}
+                required
+              />
             </label>
 
             <label htmlFor="">
-              Password
-              <input type="password" placeholder="Adicione seu link aqui" />
+              Senha
+              <input
+                type="password"
+                placeholder="Adicione seu link aqui"
+                onChange={handlePasswordChange}
+                value={password}
+                minLength={8}
+                required
+              />
             </label>
 
-            <button type="button">{title}</button>
+            <button type="submit">{title}</button>
           </form>
         </div>
       )}
