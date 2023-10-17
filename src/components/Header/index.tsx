78ea0bfafa97e4716a843d56f78961fe96a2d905
Diego Ramos <diegorbrito9@gmail.com>
2023-10-17 09:44:14 -0300

    feat: criando função para o logout

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -10,7 +10,15 @@ export function Header() {
     openRegisterModal,
     toggleOpenLoginModal,
     toggleOpenRegister,
+    isLogged,
+    setIsLogged,
   } = useModal();
+
+  function handleLogout() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    setIsLogged(false);
+  }
 
   return (
     <>
@@ -23,29 +31,45 @@ export function Header() {
       <Modal
         isOpen={openRegisterModal}
         toggleOpen={toggleOpenRegister}
-        title="Register"
+        title="Cadastrar"
       />
 
       <header className={styles.headerContainer}>
         <h1>Linkly</h1>
 
         <section className={styles.buttonContainer}>
-          <button className={styles.loginButton} onClick={toggleOpenLoginModal}>
-            Login
-            <Image
-              src="/assets/signIn-arrow.svg"
-              height={15}
-              width={15}
-              alt="arrow"
-            />
-          </button>
+          {isLogged ? (
+            <>
+              <h2 className={styles.welcomeMessage}>
+                Bem vindo <span>{localStorage.getItem('user')}</span>
+              </h2>
+              <button className={styles.logoutButton} onClick={handleLogout}>
+                Sair da conta
+              </button>
+            </>
+          ) : (
+            <>
+              <button
+                className={styles.loginButton}
+                onClick={toggleOpenLoginModal}
+              >
+                Login
+                <Image
+                  src="/assets/signIn-arrow.svg"
+                  height={15}
+                  width={15}
+                  alt="arrow"
+                />
+              </button>
 
-          <button
-            className={styles.registerButton}
-            onClick={toggleOpenRegister}
-          >
-            Register Now
-          </button>
+              <button
+                className={styles.registerButton}
+                onClick={toggleOpenRegister}
+              >
+                Cadastrar
+              </button>
+            </>
+          )}
         </section>
       </header>
     </>
